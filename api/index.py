ca81e998e1b698763fcf3a11a40723b5fee6f6e1
Aditya Sharma <asrpcp11@gmail.com>
2025-12-22 11:34:36 +0530

    Add send_chat route to handle chat messages

--- api/index.py
+++ api/index.py
@@ -5,11 +5,11 @@ from pusher import Pusher
 app = Flask(__name__, template_folder='../templates')
 
 pusher_client = Pusher(
-  app_id='2093703',
-  key='c7f2cd8e7835a9dc4425',
-  secret='22d37c4c1b5c1318c3f2',
-  cluster='ap2',
-  ssl=True
+    app_id='2093703',
+    key='c7f2cd8e7835a9dc4425',
+    secret='22d37c4c1b5c1318c3f2',
+    cluster='ap2',
+    ssl=True
 )
 
 @app.route('/')
@@ -19,6 +19,11 @@ def index():
 @app.route('/send-signal', methods=['POST'])
 def send_signal():
     data = request.json
-    room_id = data.get('room')
-    pusher_client.trigger(f'room-{room_id}', 'new-signal', data)
+    pusher_client.trigger(f"room-{data['room']}", 'new-signal', data)
+    return jsonify({"status": "success"})
+
+@app.route('/send-chat', methods=['POST'])
+def send_chat():
+    data = request.json
+    pusher_client.trigger(f"room-{data['room']}", 'new-chat', data)
     return jsonify({"status": "success"})
