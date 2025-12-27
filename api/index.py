8c00c2d4c16ed223d6b85e14766a746d3419721b
Aditya Sharma <asrpcp11@gmail.com>
2025-12-27 20:10:19 +0530

    Reformat Pusher initialization and routes

--- api/index.py
+++ api/index.py
@@ -4,12 +4,13 @@ from pusher import Pusher
 
 app = Flask(__name__, template_folder='../templates')
 
+# Initialize Pusher
 pusher_client = Pusher(
-    app_id='2093703',
-    key='c7f2cd8e7835a9dc4425',
-    secret='22d37c4c1b5c1318c3f2',
-    cluster='ap2',
-    ssl=True
+  app_id='2093703',
+  key='c7f2cd8e7835a9dc4425',
+  secret='22d37c4c1b5c1318c3f2',
+  cluster='ap2',
+  ssl=True
 )
 
 @app.route('/')
